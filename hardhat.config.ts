35259728c7e72150588780836de7fa43d49e00e7
Lorran Sutter <sutterlorran@gmail.com>
2024-11-12 18:21:19 -0500

    Amoy deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,9 @@
+import * as dotenv from 'dotenv'
+
 import { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
+
+dotenv.config()
 
 const config: HardhatUserConfig = {
     solidity: {
@@ -14,6 +18,32 @@ const config: HardhatUserConfig = {
                         enabled: true,
                         runs: 200,
                     },
+                },
+            },
+        ],
+    },
+    networks: {
+        polygon: {
+            url: process.env.POLYGON_URL || '',
+            accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+        },
+        amoy: {
+            url: process.env.AMOY_URL || '',
+            accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+        },
+    },
+    etherscan: {
+        apiKey: {
+            polygon: process.env.POLYGONSCAN_API_KEY || '',
+            polygonAmoy: process.env.POLYGONSCAN_API_KEY || '',
+        },
+        customChains: [
+            {
+                network: 'polygonAmoy',
+                chainId: 80002,
+                urls: {
+                    apiURL: 'https://api-amoy.polygonscan.com/api',
+                    browserURL: 'https://amoy.polygonscan.com/',
                 },
             },
         ],
